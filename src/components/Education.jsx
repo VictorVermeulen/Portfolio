3fb7e0b6c317c9aa2b703237e38e9ee087c88866
Victor Vermeulen <vcvermeulen@gmail.com>
2020-08-18 20:31:50 +0200

    added vue projects

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,59 +1,57 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-	root : {
-		backgroundColor : '#f9f7f7',
-		color           : '#112d4e',
-		marginBottom    : '2em',
-		padding         : '3em',
-		textAlign       : 'left'
-	}
+  root: {
+    backgroundColor: "#f9f7f7",
+    color: "#112d4e",
+    marginBottom: "2em",
+    padding: "3em",
+    textAlign: "left",
+  },
 });
 
 export default function Education() {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<div className={classes.root}>
-			<Typography variant="h4" gutterBottom>
-				Education
-			</Typography>
+  return (
+    <div className={classes.root}>
+      <Typography variant="h4" gutterBottom>
+        Education
+      </Typography>
 
-			<Typography variant="body1" gutterBottom>
-				<ul>
-					<li>
-						2009-2013: Bachelor of Science (BSc) in Economics and Business.
-						Specialization: Economics and Finance. Universiteit van Amsterdam.
-					</li>
-					<li>
-						2018-2019:{' '}
-						<a
-							href="https://www.freecodecamp.org/certification/victorvermeulen/javascript-algorithms-and-data-structures"
-							target="_blank"
-						>
-							Developer Certification JavaScript Algorithms and Data Structures.
-							FreeCodeCamp.
-						</a>
-					</li>
-					<li>
-						2019:{' '}
-						<a
-							href="https://www.freecodecamp.org/certification/victorvermeulen/responsive-web-design"
-							target="_blank"
-						>
-							Developer Certification Responsive Web Design. FreeCodeCamp.
-						</a>
-					</li>
-					<li>
-						2019:{' '}
-						<a href="https://ude.my/UC-YY49U57W" target="_blank">
-							The Complete 2020 Web Development Bootcamp. Udemy
-						</a>
-					</li>
-				</ul>
-			</Typography>
-		</div>
-	);
+      <Typography variant="body1" gutterBottom>
+        <ul>
+          <li>
+            2009-2013: Bachelor of Science (BSc) in Economics and Business.
+            Specialization: Economics and Finance. Universiteit van Amsterdam.
+          </li>
+          <li>
+            2018-2019:{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/victorvermeulen/javascript-algorithms-and-data-structures"
+              target="_blank">
+              Developer Certification JavaScript Algorithms and Data Structures.
+              FreeCodeCamp.
+            </a>
+          </li>
+          <li>
+            2019:{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/victorvermeulen/responsive-web-design"
+              target="_blank">
+              Developer Certification Responsive Web Design. FreeCodeCamp.
+            </a>
+          </li>
+          <li>
+            2019:{" "}
+            <a href="https://ude.my/UC-YY49U57W" target="_blank">
+              The Complete 2020 Web Development Bootcamp. Udemy
+            </a>
+          </li>
+        </ul>
+      </Typography>
+    </div>
+  );
 }
--- src/projects.js
+++ src/projects.js
@@ -1,59 +1,77 @@
 const projects = [
-	{
-		key: 1,
-		title: 'Meditation App',
-		image: './images/meditation.png',
-		description:
-			'I created the meditation app with React Native, using the Expo SDK. To watch a video of me explaining the app and the code, please visit the code link. If you have Android and want to download Expo Client, feel free to test the app yourself using the website link.',
-		code: 'https://vimeo.com/386471448',
-		website: 'https://expo.io/@victorvermeulen/meditation'
-	},
-	{
-		key: 2,
-		title: 'Website Tennis Trainer',
-		image: './images/tennis.jpg',
-		description:
-			'For this website I used Gatsby because the fast page load time it provides and Chakra-UI for the components. I connected the site with Contentful so the owner can easily post events himself without having to touch any code. Finally, I did host the site with Netlify which provides the owner with the option to download all form signups in Excel, which helps with efficiency.',
-		code: '',
-		website: 'https://www.jslp-academy.nl/'
-	},
+  {
+    key: 1,
+    title: "Meditation App",
+    image: "./images/meditation.png",
+    description:
+      "I created the meditation app with React Native, using the Expo SDK. To watch a video of me explaining the app and the code, please visit the code link. If you have Android and want to download Expo Client, feel free to test the app yourself using the website link.",
+    code: "https://vimeo.com/386471448",
+    website: "https://expo.io/@victorvermeulen/meditation",
+  },
+  {
+    key: 2,
+    title: "Website Tennis Trainer",
+    image: "./images/tennis.jpg",
+    description:
+      "For this website I used Gatsby because the fast page load time it provides and Chakra-UI for the components. I connected the site with Contentful so the owner can easily post events himself without having to touch any code. Finally, I did host the site with Netlify which provides the owner with the option to download all form signups in Excel, which helps with efficiency.",
+    code: "",
+    website: "https://www.jslp-academy.nl/",
+  },
 
-	{
-		key: 3,
-		title: 'Bitcoin Ticker',
-		image: './images/bitcoin.jpg',
-		description:
-			'For this Bitcoin ticker I used server side rendering with the web framework Express for Node.js. To do this, I used the API from bitcoinaverage.com. The project is deployed on Heroku.',
-		code: '',
-		website: 'https://enigmatic-meadow-57316.herokuapp.com/'
-	},
-	{
-		key: 4,
-		title: 'Signup to My Newsletter',
-		image: './images/penpaper.jpg',
-		description:
-			'This is another project I did to practise with Express, NodeJS and the use of an API (MailChimp).',
-		code: '',
-		website: 'https://young-garden-41666.herokuapp.com/'
-	},
-	{
-		key: 5,
-		title: 'Personal Portfolio',
-		image: './images/portfolio.jpg',
-		description:
-			'I decided to create this portfolio using React & Material-UI as practise.',
-		code: 'https://github.com/VictorVermeulen/Portfolio/tree/master',
-		website: ''
-	},
-	{
-		key: 6,
-		title: 'Investment Portfolio Advisor',
-		image: './images/money.jpg',
-		description:
-			'When I did some calculations for my own portfolio, I figured it would be useful and a nice project to have my own website for it. I used Bootstrap for the front end and used some basic Javascript to do the calculations for me.',
-		code: 'https://codepen.io/victorvermeulen/pen/orrLER',
-		website: 'https://codepen.io/victorvermeulen/full/orrLER'
-	}
+  {
+    key: 3,
+    title: "Bitcoin Ticker",
+    image: "./images/bitcoin.jpg",
+    description:
+      "For this Bitcoin ticker I used server side rendering with the web framework Express for Node.js. To do this, I used the API from bitcoinaverage.com. The project is deployed on Heroku.",
+    code: "",
+    website: "https://enigmatic-meadow-57316.herokuapp.com/",
+  },
+  {
+    key: 4,
+    title: "Signup to My Newsletter",
+    image: "./images/penpaper.jpg",
+    description:
+      "This is another project I did to practise with Express, NodeJS and the use of an API (MailChimp).",
+    code: "",
+    website: "https://young-garden-41666.herokuapp.com/",
+  },
+  {
+    key: 5,
+    title: "Personal Portfolio",
+    image: "./images/portfolio.jpg",
+    description:
+      "I decided to create this portfolio using React & Material-UI as practise.",
+    code: "https://github.com/VictorVermeulen/Portfolio/tree/master",
+    website: "",
+  },
+  {
+    key: 6,
+    title: "Investment Portfolio Advisor",
+    image: "./images/money.jpg",
+    description:
+      "When I did some calculations for my own portfolio, I figured it would be useful and a nice project to have my own website for it. I used Bootstrap for the front end and used some basic Javascript to do the calculations for me.",
+    code: "https://codepen.io/victorvermeulen/pen/orrLER",
+    website: "https://codepen.io/victorvermeulen/full/orrLER",
+  },
+  {
+    key: 7,
+    title: "West Park Eats",
+    image: "./images/westparkeats.png",
+    description:
+      "I made this app for a company that helps restaurants promoting their services. I used Gridsome, CMS Contentful, Vuetify and Firebase for likes & authentication.",
+    code: "",
+    website: "https://vimeo.com/423136981",
+  },
+  {
+    key: 8,
+    title: "Koken met Sien",
+    image: "./images/kokenmetsien.png",
+    description:
+      "I made this website for someone who is very passionate about cooking and taking pictures. I used Gridsome, CMS Forestry and Vuetify for this project.",
+    code: "",
+    website: "https://www.kokenmetsien.nl/",
+  },
 ];
 
 export default projects;
